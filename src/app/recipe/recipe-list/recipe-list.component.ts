0c573f8693d451fed0ba35bead88c3e59a461374
sid5233 <sudarshan.bhamare@gmail.com>
2022-02-17 23:25:59 +0530

    recipe app completed.

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
+  recipes : recipe[] = [
+    new recipe('My Recipe', 'this is dummy recipe', 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'), 
+    new recipe('My Recipe', 'this is dummy recipe', 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80')
+  
+  ];
+
+}
