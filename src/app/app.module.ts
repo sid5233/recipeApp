1e54fe1c76af6abde25175c15afd7974c897fdb6
sid5233 <sudarshan.bhamare@gmail.com>
2022-02-17 23:28:57 +0530

    recipe app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { recipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    recipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recipe/recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector:'app-recipe',
+    templateUrl:'recipe.component.html',
+    styleUrls:['./recipe.component.css']
+})
+
+export class recipeComponent implements OnInit{
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
